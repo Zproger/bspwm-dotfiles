f56d67b52d4ec03fca78016fb490bedb1e0e13f2
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2024-06-01 18:58:31 +0300

    добавлен пакет zenity для корректной работы rofi-vpn-manager

--- Builder/packages.py
+++ Builder/packages.py
@@ -15,7 +15,7 @@ BASE_PACKAGES = [
     "vlc", "usbutils", "picom", "networkmanager-openvpn", "alsa-plugins", "alsa-tools", "alsa-utils", "ffmpeg",
     "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "ueberzug", "netctl", "light", "libreoffice",
     "breeze", "vulkan-intel", "intel-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
-    "ttf-iosevka-nerd", "playerctl", "starship", "upower"
+    "ttf-iosevka-nerd", "playerctl", "starship", "upower", "zenity"
 ]
 
 AUR_PACKAGES = [
