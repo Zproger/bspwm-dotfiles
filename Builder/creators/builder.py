fdb8dddfb23f1bfb63655e2377f6675350647b15
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2024-06-06 23:14:40 +0300

    Фикс курсора (если не устанавливался на рабочем столе)

--- Builder/creators/builder.py
+++ Builder/creators/builder.py
@@ -156,3 +156,5 @@ class SystemConfiguration:
         os.system("cp -r themes ~/.themes")
         os.system("cp xinitrc ~/.xinitrc")
         os.system("cp -r bin/ ~/")
+        os.system("mkdir -p ~/.icons/default")
+        os.system("cp icons/default/index.theme ~/.icons/default/index.theme")
