448ce20006e4bcab0b1656faf7323ba48b26b4c1
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2024-06-06 15:04:09 +0300

    Добавляем тему иконок

--- Builder/packages.py
+++ Builder/packages.py
@@ -19,7 +19,7 @@ BASE_PACKAGES = [
 ]
 
 AUR_PACKAGES = [
-    "cava", "i3lock-color", "ptpython", "greenclip", "update-grub"
+    "cava", "i3lock-color", "ptpython", "greenclip", "update-grub", "tela-circle-icon-theme-dracula"
 ]
 
 
