30ad4f90beea846e4cc1ff0820457308fffa85a9
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2024-06-20 12:08:05 +0300

    Поддержка нескольких мониторов

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-#polybar example  >>/tmp/polybar1.log 2>&1 &
-polybar top -r >>/tmp/polybar1.log 2>&1 & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
 
-echo "Bars launched..."
-
+echo "Polybar запущен..."
