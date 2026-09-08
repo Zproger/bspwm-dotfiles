6f190fc24612f89187bc1e4dc73382d04661d04e
ZProger <81753080+Zproger@users.noreply.github.com>
2026-09-09 02:07:39 +0300

    Обновить версию сборки до 1.3

--- Builder/assets/startup.sh
+++ Builder/assets/startup.sh
@@ -9,6 +9,6 @@ clear
 printf "${cb} ┌─┐┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐┬┬  ┌─┐┌─┐\n"
 printf "${cb} ┌─┘├─┘├┬┘│ ││ ┬├┤ ├┬┘   │││ │ │ ├┤ ││  ├┤ └─┐\n"
 printf "${cb} └─┘┴  ┴└─└─┘└─┘└─┘┴└─  ─┴┘└─┘ ┴ └  ┴┴─┘└─┘└─┘\n\n"
-printf "${cg} Version: 1.2\n"
+printf "${cg} Version: 1.3\n"
 printf "${cg} Youtube: https://youtube.com/@zproger"
 printf "${default}\n\n"
